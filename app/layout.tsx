0a91f7186dfc918dcced89a78dcd6d7adbeef522
Daffa Hardhan <dapahardan@gmail.com>
2025-01-31 21:32:22 +0700

    Favicon Argghhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,18 @@ import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import { AuthProvider } from "./context/authcontext";
 import { CartProvider } from "./context/cartcontext";
+import { Metadata } from "next";
+
+
+
+export const metadata : Metadata = {
+  title: "TofuCraze",
+  description: "Get a Few",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
 
 export default function RootLayout({
   children,
@@ -14,9 +26,9 @@ export default function RootLayout({
       <body className="w-full h-auto">
         <AuthProvider>
           <CartProvider>
-          <Navbar />
+            <Navbar />
             <main className="flex flex-grow">{children}</main>
-          <Footer />
+            <Footer />
           </CartProvider>
         </AuthProvider>
       </body>
