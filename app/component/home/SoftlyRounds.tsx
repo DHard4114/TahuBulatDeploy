a4d3fcc433e1cb23850754cf080af27003edeb0f
Daffa Hardhan <dapahardan@gmail.com>
2025-02-06 10:04:50 +0700

    Final fitur

--- app/component/home/SoftlyRounds.tsx
+++ app/component/home/SoftlyRounds.tsx
@@ -1,18 +1,10 @@
-import Image from "next/image";
+
 import Button from "../shared/Button";
 
 const SoftlyRounds = () => {
   return (
-    <div className="relative w-full h-screen">
-      {/* Background Image */}
-      <Image
-        src="/TahuBulat4.webp"
-        alt="Softly Rounds Background"
-        layout="fill"
-        objectFit="cover"
-        priority
-      />
-
+    <div className="relative w-full h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: "url('/TahuBulat4.webp')" }}>
+      
       {/* Overlay Content */}
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-anton tracking-[0.1em] text-center px-4">
